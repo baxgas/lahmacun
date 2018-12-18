4135e87a4c504d2c3face697da3de5bae32b618c
Unknown <baxgas@gmail.com>
2018-12-18 21:09:27 +0100

    Player fixes

--- radio_BG.js
+++ radio_BG.js
@@ -2,6 +2,25 @@
  * Radio Player Script
  */
 
+function is_touch_device() {
+if(window.matchMedia("(any-pointer: coarse)").matches) {
+    // touchscreen
+    //console.log( "touch!" );
+    $("html").addClass("ismobile");
+}
+}
+
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    // console.log( "ready!" );
+    is_touch_device();
+});
+
+/**
+ * Radio Player Script
+ */
+
 var volume = 55,
     is_playing = false,
     player,
